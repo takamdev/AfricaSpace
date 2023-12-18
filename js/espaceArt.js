08a3a3282e9498ddf9982618d9006ab5c551b443
takamdev <panguslariss@gmail.com>
2023-12-18 17:31:11 +0100

    amelioration de l'espace artiste

---
+++ js/espaceArt.js
@@ -0,0 +1,21 @@
+let intputImg = document.getElementById('imgfile')
+let inputImgPorfolio = document.getElementById('imgfil')
+let imgArtiste2 = document.querySelector('.imgArtiste2 img')
+let imgArtiste1 = document.querySelector('.imgArtiste1 img')
+
+intputImg.addEventListener('input',()=>{
+    imgArtiste1.setAttribute('src',URL.createObjectURL(intputImg.files[0]))
+
+
+
+
+
+})
+inputImgPorfolio.addEventListener('input',()=>{
+    imgArtiste2.setAttribute('src',URL.createObjectURL(inputImgPorfolio.files[0]))
+
+
+
+
+
+})
