a2e07dded40d80676e98d13e98b39e305dc2341a
takamdev <panguslariss@gmail.com>
2024-01-03 22:20:06 +0100

    gestion des couleurs sur les toilles et amelioration du compteur sur le panier

--- js/interac.js
+++ js/interac.js
@@ -1,152 +1,122 @@
-document.querySelector('head').innerHTML+=`<link rel="stylesheet" href="../../css/lib/animate.min.css">`
 
-
-
-function etat1() {
-   let e1 = document.getElementById("etoile1");
-   let e2 = document.getElementById("etoile2");
-   let e3 = document.getElementById("etoile3");
-   let e4 = document.getElementById("etoile4");
-   let e5 = document.getElementById("etoile5");
-
-   if (
-      e1.style.color !== "yellow" &&
-      e2.style.color !== "yellow" &&
-      e3.style.color !== "yellow" &&
-      e4.style.color !== "yellow" &&
-      e5.style.color !== "yellow"
-   ) {
-      e1.style.color = "yellow";
-   } else if (
-      e1.style.color === "yellow" &&
-      e2.style.color === "black" &&
-      e3.style.color === "black" &&
-      e4.style.color === "black" &&
-      e5.style.color === "black"
-   ) {
-      e1.style.color = "black";
-   }
-}
-function etat2() {
-   let e1 = document.getElementById("etoile1");
-   let e2 = document.getElementById("etoile2");
-   let e3 = document.getElementById("etoile3");
-   let e4 = document.getElementById("etoile4");
-   let e5 = document.getElementById("etoile5");
-
-   if (
-      e1.style.color === "yellow" &&
-      e2.style.color !== "yellow" &&
-      e3.style.color !== "yellow" &&
-      e4.style.color !== "yellow" &&
-      e5.style.color !== "yellow"
-   ) {
-      e2.style.color = "yellow";
-   } else if (
-      e1.style.color === "yellow" &&
-      e2.style.color === "yellow" &&
-      e3.style.color === "black" &&
-      e4.style.color === "black" &&
-      e5.style.color === "black"
-   ) {
-      e2.style.color = "black";
-   }
-}
-function etat3() {
-   let e1 = document.getElementById("etoile1");
-   let e2 = document.getElementById("etoile2");
-   let e3 = document.getElementById("etoile3");
-   let e4 = document.getElementById("etoile4");
-   let e5 = document.getElementById("etoile5");
-
-   if (
-      e1.style.color === "yellow" &&
-      e2.style.color === "yellow" &&
-      e3.style.color !== "yellow" &&
-      e4.style.color !== "yellow" &&
-      e5.style.color !== "yellow"
-   ) {
-      e3.style.color = "yellow";
-   } else if (
-      e1.style.color === "yellow" &&
-      e2.style.color === "yellow" &&
-      e3.style.color === "yellow" &&
-      e4.style.color === "black" &&
-      e5.style.color === "black"
-   ) {
-      e3.style.color = "black";
-   }
-}
-function etat4() {
-   let e1 = document.getElementById("etoile1");
-   let e2 = document.getElementById("etoile2");
-   let e3 = document.getElementById("etoile3");
-   let e4 = document.getElementById("etoile4");
-   let e5 = document.getElementById("etoile5");
-
-   if (
-      e1.style.color === "yellow" &&
-      e2.style.color === "yellow" &&
-      e3.style.color === "yellow" &&
-      e4.style.color !== "yellow" &&
-      e5.style.color !== "yellow"
-   ) {
-      e4.style.color = "yellow";
-   } else if (
-      e1.style.color === "yellow" &&
-      e2.style.color === "yellow" &&
-      e3.style.color === "yellow" &&
-      e4.style.color === "yellow" &&
-      e5.style.color === "black"
-   ) {
-      e4.style.color = "black";
-   }
-}
-function etat5() {
-   let e1 = document.getElementById("etoile1");
-   let e2 = document.getElementById("etoile2");
-   let e3 = document.getElementById("etoile3");
-   let e4 = document.getElementById("etoile4");
-   let e5 = document.getElementById("etoile5");
-
-   if (
-      e1.style.color === "yellow" &&
-      e2.style.color === "yellow" &&
-      e3.style.color === "yellow" &&
-      e4.style.color === "yellow" &&
-      e5.style.color !== "yellow"
-   ) {
-      e5.style.color = "yellow";
-   } else if (
-      e1.style.color === "yellow" &&
-      e2.style.color === "yellow" &&
-      e3.style.color === "yellow" &&
-      e4.style.color === "yellow" &&
-      e5.style.color === "yellow"
-   ) {
-      e5.style.color = "black";
-   }
-}
-
-let ajoutPanier = document.querySelector(".ajoutPanier");
-let validation = document.querySelector(".validation");
+// ont recupere tout le bouttons ajouter au panier
+let ajoutPanier = document.querySelectorAll(".ajoutPanier");
+// ont recuperer le chiffre du compte de contenue dans le panier
 let nombrePieces = document.querySelector(".nombrePieces");
 let compteur = 0;
 
-ajoutPanier.addEventListener("click", () => {
-   compteur++;
-   nombrePieces.textContent = compteur;
-   if (compteur === 5) {
-      validation.innerHTML += `<p class="animate__animated animate__flash  text-danger display-6 erreurNmbreArti">vous ne pouvez pas acheter plus de 5 oeuvres d'art</p>`;
-      //
-      setTimeout(() => {
-         compteur = 0;
-         nombrePieces.textContent = compteur;
-         location.reload()
-      }, 2000);
-   }
-   
+// COMPTEUR DU NOMBRE D4ATICLE DANS LE PANIER
 
-  
+// parcours de tout les bouttons ajouter au panier
+ajoutPanier.forEach((Element) => {
+   //ajout d'un ecouteur sur chaque boutton
+   Element.addEventListener("click", () => {
+      compteur++;
+      nombrePieces.textContent = compteur;
+   });
 });
 
+// recuperer tout les p qui contiennent les etoilles
+let PEtoiles = document.querySelectorAll(".validation p:nth-child(1)");
+// ont parcour le p
+PEtoiles.forEach((PEtoiles) => {
+   // ont recupere les etoilles
+   let etoilles = [...PEtoiles.querySelectorAll(".etoile")];
+   etoilles.forEach((etoille) => {
+      // ont ajoute la classe couleur2 sur chaque etoile pour qu'il ait ue couleur noir
+      etoille.classList.add('couleur2')
+      //ajout d'un ecouteur sur chaque etoille
+      etoille.addEventListener("click", () => {
+        
+         if (etoilles.indexOf(etoille) === 0) {
+            if (
+               getStyle(etoilles[1]) === true &&
+               getStyle(etoilles[2]) === true &&
+               getStyle(etoilles[3]) === true &&
+               getStyle(etoilles[4]) === true
+            ) {
+               if(etoille.classList.contains('couleur2')){
+                  etoille.classList.remove('couleur2')
+               }else{
+                  etoille.classList.add('couleur2')
+               }
+               etoille.classList.toggle("couleur");
+            }
+         }
+
+
+         if (etoilles.indexOf(etoille) === 1) {
+            if (
+               getStyle(etoilles[0]) === false &&
+               getStyle(etoilles[2]) === true &&
+               getStyle(etoilles[3]) === true &&
+               getStyle(etoilles[4]) === true
+            ) {
+            if(etoille.classList.contains('couleur2')){
+               etoille.classList.remove('couleur2')
+            }else{
+               etoille.classList.add('couleur2')
+            }
+            etoille.classList.toggle("couleur");
+            }
+         }
+         
+         if (etoilles.indexOf(etoille) === 2) {
+            if (
+               getStyle(etoilles[0]) === false &&
+               getStyle(etoilles[1]) === false &&
+               getStyle(etoilles[3]) === true &&
+               getStyle(etoilles[4]) === true
+            ) {
+               if(etoille.classList.contains('couleur2')){
+                  etoille.classList.remove('couleur2')
+               }else{
+                  etoille.classList.add('couleur2')
+               }
+               etoille.classList.toggle("couleur");
+            }
+         }
+
+         if (etoilles.indexOf(etoille) === 3) {
+            if (
+               getStyle(etoilles[0]) === false &&
+               getStyle(etoilles[1]) === false &&
+               getStyle(etoilles[2]) === false &&
+               getStyle(etoilles[4]) === true
+            ) {
+               if(etoille.classList.contains('couleur2')){
+                  etoille.classList.remove('couleur2')
+               }else{
+                  etoille.classList.add('couleur2')
+               }
+               etoille.classList.toggle("couleur");
+            }
+         }
+
+
+         if (etoilles.indexOf(etoille) === 4) {
+            if (
+               getStyle(etoilles[0]) === false &&
+               getStyle(etoilles[1]) === false &&
+               getStyle(etoilles[2]) === false &&
+               getStyle(etoilles[3]) === false
+            ) {
+               if(etoille.classList.contains('couleur2')){
+                  etoille.classList.remove('couleur2')
+               }else{
+                  etoille.classList.add('couleur2')
+               }
+               etoille.classList.toggle("couleur");
+            }
+         }
+      });
+   });
+});
+// fonction qui determine si l'etoille a la couleur noir ou jaune
+function getStyle(element) {
+   if (element.classList.contains('couleur2')) {
+      return true;
+   } else {
+      return false;
+   }
+}
