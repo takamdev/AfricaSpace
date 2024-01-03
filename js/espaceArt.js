c90c0432e82974b6599e0d88fae6d8f8a90ebaed
takamdev <panguslariss@gmail.com>
2024-01-03 11:47:45 +0100

    dinamisation del'espace artiste

--- js/espaceArt.js
+++ js/espaceArt.js
@@ -5,17 +5,7 @@ let imgArtiste1 = document.querySelector('.imgArtiste1 img')
 
 intputImg.addEventListener('input',()=>{
     imgArtiste1.setAttribute('src',URL.createObjectURL(intputImg.files[0]))
-
-
-
-
-
 })
 inputImgPorfolio.addEventListener('input',()=>{
     imgArtiste2.setAttribute('src',URL.createObjectURL(inputImgPorfolio.files[0]))
-
-
-
-
-
 })
